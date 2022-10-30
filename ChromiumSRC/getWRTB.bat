ffdebe59b7c14bde1a04d52855397a85d53201a1
Tangram <tangramteam@outlook.com>
2022-10-30 15:49:33 +0800

    new getWRTB batch file

--- ChromiumSRC/getWRTB.bat
+++ ChromiumSRC/getWRTB.bat
@@ -1,2 +1,2 @@
 @echo off
-git fetch origin tag 106.0.5249.%1 && git checkout -B M106.5249.%1 106.0.5249.%1&& gclient sync -D && git cherry-pick Base&&..\b1.sh&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
+git fetch origin tag 106.0.5249.%1 && git checkout -B M106.5249.%1 106.0.5249.%1&& gclient sync -D && git cherry-pick Base&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
