a128aecd08c6bbe31f1927b5608efeac5cebabfe
Tangram <tangramteam@outlook.com>
2023-01-11 05:00:17 +0800

    update to M109

--- ChromiumSRC/getWRTB.bat
+++ ChromiumSRC/getWRTB.bat
@@ -1,2 +1,2 @@
 @echo off
-git fetch origin tag 106.0.5249.%1 && git checkout -B M106.5249.%1 106.0.5249.%1&& gclient sync -D && git cherry-pick Base&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
+git fetch origin tag 109.0.5414.%1 && git checkout -B M109.5414.%1 109.0.5414.%1&& gclient sync -D && git cherry-pick Base&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
