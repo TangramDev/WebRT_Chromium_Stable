c6bbdc7e84756d643f93422cd0ad05e241b2d11d
Tangram <tangramteam@outlook.com>
2023-01-11 05:05:03 +0800

    M109

--- ChromiumSRC/getbranch2.bat
+++ ChromiumSRC/getbranch2.bat
@@ -1,2 +1,2 @@
 @echo off
-git fetch origin 107.0.5304.%1 &&git checkout -b %2 107.0.5304.%1 && git branch --set-upstream-to=branch-heads/5304 %2 && gclient sync -D
+git fetch origin 109.0.5414.%1 &&git checkout -b %2 109.0.5414.%1 && git branch --set-upstream-to=branch-heads/5414 %2 && gclient sync -D
